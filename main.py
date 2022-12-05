1216557ab87338c1e9b97e31de46e2486ce0b6a6
unknown <condoluci.nico@gmail.com>
2022-12-04 23:39:16 -0300

    Integración de detector de patentes / ordenamiento de código

--- main.py
+++ main.py
@@ -7,12 +7,82 @@ from geopy.distance import geodesic
 import os
 import folium
 import webbrowser
+# Librerías para la detección de patente
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\54113\AppData\Local\Tesseract-OCR\tesseract.exe'
+
+# CONSTANTES
+RECLAMOS: str = 'reclamosad.csv'
+CAPTURA: str = 'robados.txt'
+RIVER_LOC: list = ['-34.545173623765045','-58.44980708914722']
+BOCA_LOC:list = ['-34.63547846773916','-58.36471338729659']
+MESES: list = ["Enero", "Febrero", "Marzo", "Abril", 
+    "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+    "Octubre", "Noviembre", "Diciembre" ]
+
+# Detectar y escribir patente
+def detectar_patente(ruta_img: str)-> str:
+
+    img = cv2.imread(ruta_img,cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (600,400))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    gray = cv2.bilateralFilter(gray, 13, 15, 15) 
+
+    edged = cv2.Canny(gray, 30, 200) 
+    contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+    screenCnt = None
+
+    for c in contours:
+        
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+    
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+
+    if screenCnt is None:
+        detected = 0
+        print ("No contour detected")
+    else:
+        detected = 1
+
+    if detected == 1:
+        cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+
+    mask = np.zeros(gray.shape,np.uint8)
+    new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+    new_image = cv2.bitwise_and(img,img,mask=mask)
+
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+
+    text = pytesseract.image_to_string(Cropped, config='--psm 11')
+    print("programming_fever's License Plate Recognition\n")
+    print("Detected license plate Number is:",text)
+    img = cv2.resize(img,(500,300))
+    Cropped = cv2.resize(Cropped,(400,200))
+    cv2.imshow('car',img)
+    cv2.imshow('Cropped',Cropped)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return text
+
 # crear nuevo CSV
-def transcrpicion_audio (ruta_audio: str)->str:
+def transcrpicion_audio(ruta_audio: str)->str:
     """
-Pre: Require la ruta absoluta de un audio en formato wav
-Post: Devuelve la trancricion del audio como str
-"""
+    Pre: Require la ruta absoluta de un audio en formato wav
+    Post: Devuelve la trancricion del audio como str
+    """
     r = sr.Recognizer()
     transcripcion: str = ""
     with sr.AudioFile(ruta_audio) as source:
@@ -25,12 +95,14 @@ Post: Devuelve la trancricion del audio como str
         transcripcion= "Google Speech Recognition no pudo entender el audio"
     except sr.RequestError as e:
         transcripcion ="No se pudieron solicitar los resultados de Google Speech Recognition service; {0}".format(e)
+
     return transcripcion
+
 def leerArchivo(nom_arc:str,lista:list,separador:str):
     """
-Pre: Require el nombre del archivo como str, una lista vacia donde guardar los datos y caracter de serparacion como str
-Post: Al ser un procedimiento no posee, guarda los datos leído del archivo en una lista vacía
-"""
+    Pre: Require el nombre del archivo como str, una lista vacia donde guardar los datos y caracter de serparacion como str
+    Post: Al ser un procedimiento no posee, guarda los datos leído del archivo en una lista vacía
+    """
     try:
         file = open(nom_arc,'r',encoding = "UTF-8")
         for linea in file:
@@ -41,21 +113,23 @@ Post: Al ser un procedimiento no posee, guarda los datos leído del archivo en u
         file.close()
     except IOError:
         print("Se producjo un error de entrada y salida del archivo")
+
 def crear_csv(reclamos_datos_nuevos: list)->None:
- """
-Pre: Require un lista con los datos de los reclamos ya procesados
-Post: Al ser un procedimiento no posee, crea un archivo csv con eso datos.
-"""
+    """
+    Pre: Require un lista con los datos de los reclamos ya procesados
+    Post: Al ser un procedimiento no posee, crea un archivo csv con eso datos.
+    """
     with open("reclamos_nuevo.csv", 'w', newline='', encoding="UTF-8") as reclamos:
         csv_writer = csv.writer(reclamos, delimiter=',', quotechar='"', quoting= csv.QUOTE_NONNUMERIC)
         csv_writer.writerow(["Timestamp", "Telefono", "Direccion", "Localidad", "Provincia", "Patente", "Descripcion texto", "Descripcion audio"])
         for reclamo in reclamos_datos_nuevos:
             csv_writer.writerow(reclamo)
+
 def coordenadasADireccion(lat:float,long:float)->list:
     """
-Pre: Requiere dos numero flotante, el primero sera la longitud, el segundo la latitud.
-Post: Devuelve una lista con la direccion, la localidad, y la provincia
-"""
+    Pre: Requiere dos numero flotante, el primero sera la longitud, el segundo la latitud.
+    Post: Devuelve una lista con la direccion, la localidad, y la provincia
+    """
     localidad: str = ''
     localizador = Nominatim(user_agent="khasi@gmail.com")
     ubicacion = localizador.reverse(str(lat)+' , '+str(long))
@@ -77,19 +151,19 @@ def reconocimiento_patente(path_img: str)->str:
     
 def dentroDeLaDistancia(direcc1:list,direcc2:list,distan:int):
     """
-Pre: ingresas dos direcciones en forma de lista con dos elementos string, y el radio un int
-Post: retorna un bool que indica si las direcciones poseen una distancia menor o igual a la indicada en el radio
-"""
+    Pre: ingresas dos direcciones en forma de lista con dos elementos string, y el radio un int
+    Post: retorna un bool que indica si las direcciones poseen una distancia menor o igual a la indicada en el radio
+    """
     dist = geodesic(direcc1,direcc2).km
     esta_dentro_de_la_distancia: bool = dist<=distan
     return esta_dentro_de_la_distancia
 #Listar centro
 def dentroDelCuadrante(coord:list)->bool:
     """
-Pre:Requiere un lista con dos elementos str primero es latitud y el segundo es longitud
-Post:Devuelve un boleano con True si cumple las condicion de estar dentro del cuadrante
-"""
-#Pequeña introduccion teorica -> https://acortar.link/FdUE6k
+    Pre:Requiere un lista con dos elementos str primero es latitud y el segundo es longitud
+    Post:Devuelve un boleano con True si cumple las condicion de estar dentro del cuadrante
+    """
+    #Pequeña introduccion teorica -> https://acortar.link/FdUE6k
     intersec1 = ['-34.59931087515444', '-58.39337992076712']
     #Callao y Cordoba
     intersec2 = ['-34.609349307168664', '-58.39216124478244']
@@ -109,23 +183,25 @@ Post:Devuelve un boleano con True si cumple las condicion de estar dentro del cu
     division = (suma1+suma3)/(suma2+suma4)
     en_el_cuadrante: bool = division<=1.0
     return en_el_cuadrante
-#robados
+    #robados
+
 def compararDatos(datos1:list,datos2:list)->list:
     """
-Pre: Requiere dos listas, la primera es una general, la segunda es especifica.
-Post: Devuelve una lista que posee los datos en comun que tiene la primera lista con la segunda.
-"""
+    Pre: Requiere dos listas, la primera es una general, la segunda es especifica.
+    Post: Devuelve una lista que posee los datos en comun que tiene la primera lista con la segunda.
+    """
     elementos_en_comun:list = []
     for x in range(len(datos1)):
         if datos1[x][5] in datos2:
             coincidentes = datos1[x][5]
             elementos_en_comun.append(coincidentes)
     return elementos_en_comun
+
 def mostrarAlerta(pat:str,tiempo:str,ubicacion:list):
     """
-Pre: Requiete la patente como str, la fecha como str, y una lista con direccion, localidad, provincia
-Post: Imprime un cartel en la terminal en color rojo, con una alerta
-"""
+    Pre: Requiete la patente como str, la fecha como str, y una lista con direccion, localidad, provincia
+    Post: Imprime un cartel en la terminal en color rojo, con una alerta
+    """
     c:str = "—"*77
     print("\033[0;31m"+c+"\033[0;m")
     print("\033[0;31m"+"ALERTA"+"\033[0;m")
@@ -133,11 +209,12 @@ Post: Imprime un cartel en la terminal en color rojo, con una alerta
     print(f'Su ubicación es {ubicacion[0]} en {ubicacion[1]}, {ubicacion[2]}')
     print(f"El movil con patente {pat},tiene un pedido de captura ")
     print("\033[0;31m"+c+"\033[0;m")
+
 def infoAlerta(lista_robados:list,info_file:list):
     """
-Pre: Requiere una lista con las patentes coincidentes con el pedido de captura, y requiere los datos procesados de las denuncias
-Post:  Es un procedimiento que imprime en pantalla como alerta los datos de todas la denucias correspondientes a una patente con pedido de captura 
-"""
+    Pre: Requiere una lista con las patentes coincidentes con el pedido de captura, y requiere los datos procesados de las denuncias
+    Post:  Es un procedimiento que imprime en pantalla como alerta los datos de todas la denucias correspondientes a una patente con pedido de captura 
+    """
     fechas: list = []
     ubicaciones: list = []
     for i in range(len(info_file)):
@@ -156,28 +233,30 @@ Post:  Es un procedimiento que imprime en pantalla como alerta los datos de toda
 #mostrar foto y mapa
 def mostrarImg(ruta:str):
     """
-Pre: Requiere la ruta de la img a mostrar
-Post: Es un procedimiento que muestra en pantalle una imagen
-"""
+    Pre: Requiere la ruta de la img a mostrar
+    Post: Es un procedimiento que muestra en pantalle una imagen
+    """
     #archivo = 'prueba.jpg'
     #directorio = os.getcwd()
     #ruta = os.path.join(directorio, archivo)
     img = mpimg.imread(ruta)
     imgplot = plt.imshow(img)
     plt.show()
+
 def mostrarMapa(coordenada:list,patente:str):
     """
-Pre: Requiere las coordenadas a mostrar y una decripcion de una o dos palabrar sobre dicha coordenada
-Post: Traza un mapa, e inserta en el un puntero en la localizacion de las coordenadas ingresadas,luego abre dicho mapa en el navegador
-"""
+    Pre: Requiere las coordenadas a mostrar y una decripcion de una o dos palabrar sobre dicha coordenada
+    Post: Traza un mapa, e inserta en el un puntero en la localizacion de las coordenadas ingresadas,luego abre dicho mapa en el navegador
+    """
     ubicacion: list = [float(coordenada[0]),float(coordenada[1])]
     mapa=folium.Map(location=ubicacion)
     mapa.add_child(folium.Marker(location=ubicacion,popup=patente,icon=folium.Icon(color='green')))
     mapa.save('mapa.html')
     archivo = 'mapa.html'
     directorio = os.getcwd()
-    ruta = os.path.join(directorio, archivo)#El archivo se crea automaticamente y se guarda en la misma carpeta del presente programa
+    ruta = os.path.join(directorio, archivo) # El archivo se crea automaticamente y se guarda en la misma carpeta del presente programa
     webbrowser.open(ruta)
+
 def mostrarDatosPatente(archivo1:list,archivo2:list):
     time: str = ''
     tel:str = ''
@@ -203,45 +282,49 @@ def main()->None:
     datos_reclamos: list = []
     datos_reclamos_nuevo: list = []
     pedido_captura: list = []
-    RECLAMOS: str = 'reclamosad.csv'
-    CAPTURA: str = 'robados.txt'
-    leerArchivo(reclamos,datos_reclamos,';')
-    leerArchivo(captura,pedido_captura,'\n')
-    RIVER_LOC: list = ['-34.545173623765045','-58.44980708914722']
-    BOCA_LOC:list = ['-34.63547846773916','-58.36471338729659']
+    
+    leerArchivo(RECLAMOS,datos_reclamos,';')
+    leerArchivo(CAPTURA,pedido_captura,'\n')
+
     print("Creando nuevo listado")
-    MESES: list = ["Enero", "Febrero", "Marzo", "Abril", 
-        "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
-        "Octubre", "Noviembre", "Diciembre" ]
     print("Inicio de programa")
     print("Prosesado datos y generando nuevo archivo fortmato csv")
+
     for reclamo in datos_reclamos:
         # (Timestamp, Teléfono_celular, coord_latitud, coord_long, ruta_foto, descripción texto,ruta_audio)
+        print(reclamo)
         if reclamo != datos_reclamos[0]:
             ubicacion: list = coordenadasADireccion(reclamo[2],reclamo[3])
-            patente: str = reconocimiento_patente(os.path.abspath(reclamo[4]))ruta_relativa
+            print("Ruta auto: ", os.path.abspath(reclamo[4]))
+            patente: str = detectar_patente(os.path.abspath(reclamo[4]))
             descripcion_audio: str = transcrpicion_audio(os.path.abspath(reclamo[6]))
             datos_reclamos_nuevo.append([reclamo[0],reclamo[1],ubicacion[0], ubicacion[1], ubicacion[2], patente, reclamo[5], descripcion_audio])
+            print(reclamo)
     crear_csv(datos_reclamos_nuevo)
     print("Listando reclamos a 1 Km. del estadio River Plate ")
+
     for linea in datos_reclamos:
         if linea != datos_reclamos[0]:
             coord: list = [linea[2],linea[3]]
             unix= linea[0].split(' ')
             fecha = unix[0]
-            cercano: bool = dentroDeLaDistancia(river_loc,coord,1)
+            cercano: bool = dentroDeLaDistancia(RIVER_LOC,coord,1)
             if cercano:
-                print(f"Se realizó un reclamo para la patente (ingresar pat) el día {fecha} en (ingresal loc)")#falta completar info
+                print(f"Se realizó un reclamo para la patente (ingresar pat) el día {fecha} en (ingresal loc)") #falta completar info
+
     print("Listando reclamos a 1 Km. del estadio Boca Juniors")
+
     for linea in datos_reclamos:
         if linea != datos_reclamos[0]:
             coord = [linea[2],linea[3]]
             unix= linea[0].split(' ')
             fecha = unix[0]
-            cercano = dentroDeLaDistancia(boca_loc,coord,1)
+            cercano = dentroDeLaDistancia(BOCA_LOC,coord,1)
             if cercano:
                 print(f"Se realizó un reclamo para la patente el día {fecha} en")#falta completar info
+
     print("Listando reclamos en el centro de la ciudad")
+
     for linea in datos_reclamos:
         if linea != datos_reclamos[0]:
             coord = [linea[2],linea[3]]
@@ -250,25 +333,27 @@ def main()->None:
             cercano = dentroDelCuadrante(coord)#Aprovechando un bool ya definido
             if cercano:
                 print(f"Se realizó un reclamo para la patente el día {fecha} en el centro de CABA")#falta completar informacion de la patente
+
     print("Alertas de patentes con pedidos de captura")
     sospechosos: list = compararDatos(pedido_captura,datos_reclamos_nuevo) #Falta probar
     infoAlerta(sospechosos,datos_reclamos_nuevo) #falta informacion para probar la funcion
     print("Ingresar una patente y obtener su imagen y ubicacion en un mapa")
     mostrarDatosPatente(datos_reclamos,datos_reclamos_nuevo)#pendiente por comprobar que funciona
     print("Generando grafico de reclamos mensuales")
-    meses: list = ["Enero", "Febrero", "Marzo", "Abril", 
-        "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
-        "Octubre", "Noviembre", "Diciembre" ]
+
     cantxmeses: list = []
+
     for mes in range (1,13):
         cantxmes: int = 0
         for reclamo in datos_reclamos_nuevo:
             if mes ==  int(reclamo[0][5:7]):
                 cantxmes += 1
         cantxmeses.append(cantxmes)
+
     fig, ax = plt.subplots()
-    ax.bar(meses, cantxmeses)
+    ax.bar(MESES, cantxmeses)
     plt.show()
+
 main()
 
     
